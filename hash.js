088f1c62aec283dc49b533e0efb8b0719cfab590
Pascal <pascal@4mile.io>
2018-03-08 23:12:37 -0500

    looker_render

--- hash.js
+++ hash.js
@@ -1,15 +1,29 @@
-const crypto = require('crypto')
-const fs = require('fs')
+function log() {
+    console.log.apply(console, arguments)
+}
 
-const file = fs.readFileSync('./download.png')
+// hash test
+// const crypto = require('crypto')
+// const fs = require('fs')
 
-const hash = crypto.createHash('sha256')
-hash.on('readable', () => {
-    const data = hash.read()
-    if (data) {
-        console.log(data.toString('hex'))
-    }
-})
+// const file = fs.readFileSync('./download.png')
 
-hash.write(file)
-hash.end()
+// const hash = crypto.createHash('sha256')
+// hash.on('readable', () => {
+//     const data = hash.read()
+//     if (data) {
+//         console.log(data.toString('hex'))
+//     }
+// })
+
+// hash.write(file)
+// hash.end()
+
+
+
+// URL parser
+const url = require('url')
+const look_url = 'https://4mile.looker.com/looks/7?qid=M4jjgyvm3oBA23u57Ame5K'
+const u = url.parse(look_url)
+
+log(u)
--- src/actions/ibm_data_catalog/ibm_data_catalog.ts
+++ src/actions/ibm_data_catalog/ibm_data_catalog.ts
@@ -3,6 +3,7 @@ import * as stream from "stream"
 import * as req from "request"
 import * as reqPromise from "request-promise-native"
 import * as crypto from 'crypto'
+import * as url from 'url'
 
 const BEARER_TOKEN_URI = 'https://iam.ng.bluemix.net/identity/token'
 const BASE_URL = 'https://catalogs-yp-prod.mybluemix.net:443/v2'
@@ -285,7 +286,60 @@ export class IbmDataCatalogAssetAction extends Hub.Action {
   }
 
   async getLookerLookPng(transaction: Transaction) {
-    log('getLookerLookPng', transaction)
+    log('getLookerLookPng')
+
+    const render_id = await this.renderLookerPng(transaction)
+    log('render_id', render_id)
+  }
+
+  getLookerRenderUrl(transaction: Transaction) {
+    const item_url = (
+      transaction.request.scheduledPlan
+      && transaction.request.scheduledPlan.url
+    )
+    if (! item_url) return
+
+    const render_url = url.parse(item_url)
+
+    return render_url
+  }
+
+  async renderLookerPng(transaction: Transaction) {
+    // const {
+    //   looker_api_url,
+    //   looker_api_client_id,
+    //   looker_api_client_secret,
+    // } = transaction.request.params
+
+    const render_url = this.getLookerRenderUrl(transaction)
+    log('render_url', render_url)
+
+
+    // const options = {
+    //   method: 'POST',
+    //   uri: `${looker_api_url}/render_tasks/?client_id=${looker_api_client_id}&client_secret=${looker_api_client_secret}`,
+    //   headers: {
+    //     'Accept': 'application/json',
+    //   },
+    //   json: true
+    // }
+
+    return new Promise<string>((resolve, reject) => {
+      resolve('hey')
+      // reqPromise(options)
+      // .then(response => {
+      //   try {
+      //     if (response.access_token) {
+      //       log('looker_token received')
+      //       return resolve(response.access_token)
+      //     }
+      //     throw new Error('response does not include access_token')
+      //   } catch(err) {
+      //     reject(err)
+      //   }
+      // })
+      // .catch(reject)
+    })
   }
 
   async uploadPngToIbmCos(png_buffer: Buffer, transaction: Transaction) {
@@ -612,75 +666,6 @@ export class IbmDataCatalogAssetAction extends Hub.Action {
 
     })
   }
-
-  // async xform(request: Hub.ActionRequest) {
-  //   const form = new Hub.ActionForm()
-  //   const channels = await this.usableChannels(request)
-
-  //   form.fields = [{
-  //     description: "Name of the Slack channel you would like to post to.",
-  //     label: "Share In",
-  //     name: "channel",
-  //     options: channels.map((channel) => ({ name: channel.id, label: channel.label })),
-  //     required: true,
-  //     type: "select",
-  //   }, {
-  //     label: "Comment",
-  //     type: "string",
-  //     name: "initial_comment",
-  //   }, {
-  //     label: "Filename",
-  //     name: "filename",
-  //     type: "string",
-  //   }]
-
-  //   return form
-  // }
-
-  // async usableChannels(request: Hub.ActionRequest) {
-  //   let channels = await this.usablePublicChannels(request)
-  //   channels = channels.concat(await this.usableDMs(request))
-  //   return channels
-  // }
-
-  // async usablePublicChannels(request: Hub.ActionRequest) {
-  //   return new Promise<Channel[]>((resolve, reject) => {
-  //     const slack = this.slackClientFromRequest(request)
-  //     slack.channels.list({
-  //       exclude_archived: 1,
-  //       exclude_members: 1,
-  //     }, (err: any, response: any) => {
-  //       if (err || !response.ok) {
-  //         reject(err)
-  //       } else {
-  //         const channels = response.channels.filter((c: any) => c.is_member && !c.is_archived)
-  //         const reformatted: Channel[] = channels.map((channel: any) => ({ id: channel.id, label: `#${channel.name}` }))
-  //         resolve(reformatted)
-  //       }
-  //     })
-  //   })
-  // }
-
-  // async usableDMs(request: Hub.ActionRequest) {
-  //   return new Promise<Channel[]>((resolve, reject) => {
-  //     const slack = this.slackClientFromRequest(request)
-  //     slack.users.list({}, (err: any, response: any) => {
-  //       if (err || !response.ok) {
-  //         reject(err)
-  //       } else {
-  //         const users = response.members.filter((u: any) => {
-  //           return !u.is_restricted && !u.is_ultra_restricted && !u.is_bot && !u.deleted
-  //         })
-  //         const reformatted: Channel[] = users.map((user: any) => ({ id: user.id, label: `@${user.name}` }))
-  //         resolve(reformatted)
-  //       }
-  //     })
-  //   })
-  // }
-
-  // private slackClientFromRequest(request: Hub.ActionRequest) {
-  //   return new WebClient(request.params.slack_api_token!)
-  // }
 
 }
 
